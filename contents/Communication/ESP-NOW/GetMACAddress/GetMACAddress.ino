a8dcd40bfac08f326d6cdb576bbc3086b11933fc
JHathway <joehathway99@gmail.com>
2026-01-19 16:52:55 +0000

    Add GetMACAddress.ino

---
+++ contents/Communication/ESP-NOW/GetMACAddress/GetMACAddress.ino
@@ -0,0 +1,21 @@
+//==============================================================
+// File:    GetMACAddress.ino
+//
+// Author:  J. Hathway 2026
+//
+// Description:
+//     - This sketch will print the device MAC address to the
+//       serial monitor every 2 seconds.
+//==============================================================
+
+#include <WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  Serial.println("MAC Address:");
+  Serial.println(WiFi.macAddress());
+  delay(2000);
+}
